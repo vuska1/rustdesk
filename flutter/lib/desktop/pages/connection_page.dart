8d82221923c737cee8d7e5588e42b301332c89f1
vuska1 <162448637+vuska1@users.noreply.github.com>
2024-04-11 21:25:27 +0200

    Update connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -39,7 +39,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
   double? get height => bind.isIncomingOnly() ? null : em * 3;
 
   void onUsePublicServerGuide() {
-    const url = "https://rustdesk.com/pricing.html";
+    const url = "https://www.tuhh.de/rzt/services/institute";
     canLaunchUrlString(url).then((can) {
       if (can) {
         launchUrlString(url);
@@ -92,7 +92,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
                       children: [
                         Flexible(
                           child: Text(
-                            translate('setup_server_tip'),
+                            "TUHH-RustDesk-Server",
                             style: TextStyle(
                                 decoration: TextDecoration.underline,
                                 fontSize: em),

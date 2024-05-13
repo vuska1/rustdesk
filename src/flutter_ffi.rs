c34b7b85a2fc38231f71771830a299a094320797
vuska1 <162448637+vuska1@users.noreply.github.com>
2024-05-13 15:26:21 +0200

    Update flutter_ffi.rs

--- src/flutter_ffi.rs
+++ src/flutter_ffi.rs
@@ -1251,10 +1251,10 @@ pub fn main_get_last_remote_id() -> String {
 }
 
 pub fn main_get_software_update_url() -> String {
-    if get_local_option("enable-check-update".to_string()) != "N" {
-        crate::common::check_software_update();
-    }
-    crate::common::SOFTWARE_UPDATE_URL.lock().unwrap().clone()
+    // if get_local_option("enable-check-update".to_string()) != "N" {
+    //     crate::common::check_software_update();
+    // }
+    // crate::common::SOFTWARE_UPDATE_URL.lock().unwrap().clone()
 }
 
 pub fn main_get_home_dir() -> String {
